6f4348ba7af6b8599f76fa13bb9d49aebfad7d7a
chethansai999 <50831048+chethansai999@users.noreply.github.com>
2020-04-16 12:10:52 +0530

    Add files via upload

---
+++ abc.java
@@ -0,0 +1,16 @@
+package chethanjavaproject;
+
+public class abc 
+{
+	public static void main(String[] args) 
+	{
+		for (int j=65;j<=67;j++)
+		{
+			for (int i=65;i<=j;i++)
+		{
+			System.out.print((char)i + " ");
+		}
+			System.out.println();	
+		}
+	}
+}
---
+++ demo.java
@@ -0,0 +1,14 @@
+package chethanjavaproject;
+
+public class demo 
+	{
+		public static void main(String args[])
+		{
+			int i = 15;
+			int j=0;
+			 j=i>10?0:1;
+			System.out.println(j);
+		}
+			
+
+}
