5292cc76c6f4dc18df2ccd993e171850bf47faeb
chethansai999 <50831048+chethansai999@users.noreply.github.com>
2020-04-16 12:16:08 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,38 @@
+package chethanjavaproject;
+
+public class square 
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=4;i++)
+		{
+			System.out.print("$"+ " ");
+		}
+		System.out.println();
+		for(int i=1;i<5;i++)
+		{
+			if(i==1||i==4)
+			{
+				System.out.print("$"+" ");
+			}
+			else
+				System.out.print("  ");
+		}
+		System.out.println();
+		for(int i=1;i<5;i++)
+		{
+			if(i==1||i==4)
+			{
+				System.out.print("$"+" ");
+			}
+			else
+				System.out.print("  ");
+		}
+		System.out.println();
+		for(int i=1;i<=4;i++)
+		{
+			System.out.print("$"+ " ");
+		}
+
+	}
+}
